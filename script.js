899326bbc2d051e2bc8b452f6f451fcdd92dc322
Chichi3779 <johnrosey4@gmail.com>
2025-06-08 23:17:54 +0100

    captcha generator

---
+++ script.js
@@ -0,0 +1,34 @@
+//CAPTCHA PROGRAM
+
+const captchaText = document.getElementById("captc");
+const refreshBtn = document.getElementById("refresh");
+const submitBtn = document.getElementById("submit");
+const input = document.getElementById("userInput");
+const result = document.getElementById("validation-result");
+
+
+//To generate the captcha
+function generateCaptcha() {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let captcha = "";
+  for (let i = 0; i < 6; i++) {
+    captcha += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  captchaText.textContent = captcha;
+}
+
+generateCaptcha();
+
+// Refresh button
+refreshBtn.addEventListener("click", generateCaptcha);
+
+// Submit button
+submitBtn.addEventListener("click", () => {
+  if (input.value.toUpperCase().trim() === captchaText.textContent) {
+    result.textContent = "Entered captcha is correct ✅";
+    result.style.color = "green";
+  } else {
+    result.textContent = "Entered captcha is incorrect ❌";
+    result.style.color = "rgb(235, 29, 29)";
+  }
+});
